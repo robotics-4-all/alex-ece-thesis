c5de2839585c8ac25901dd94d6dca3dcb810d810
Konstantinos Panayiotou <klpanagi@gmail.com>
2023-05-23 21:00:50 +0300

    Initialization script. Currently downloads the kafka-connect-redis package

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+KAFKA_CONNECT_REDIS_ZIP_URL=https://d1i4a15mxbxib1.cloudfront.net/api/plugins/jcustenborder/kafka-connect-redis/versions/0.0.4/jcustenborder-kafka-connect-redis-0.0.4.zip
+
+mkdir -p connect/redis-connector
+
+cd ./connect/redis-connector && \
+    wget KAFKA_CONNECT_REDIS_ZIP_URL && unzip jcustenborder-kafka-connect-redis-0.0.4.zip
